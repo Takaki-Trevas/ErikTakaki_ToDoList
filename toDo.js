2522c142c8310772537977883629fd4dd6ec9d31
Takaki-Trevas <takakitrevas@gmail.com>
2025-06-17 08:55:22 -0300

    update2-html and js

---
+++ toDo.js
@@ -0,0 +1,17 @@
+let tasks = [];
+
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+function removeCompleted(){
+    
+}
+function addTasks(){
+
+}
+function showTasks(){
+
+}
+function updateTasks(){
+
+}
